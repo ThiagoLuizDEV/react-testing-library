79837b45f32d8ec646080408653419c8282dbb4c
Thiago <thiago-470@outlook.com>
2022-11-22 18:25:44 -0300

    requisito 05

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,50 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('testa o titulo', () => {
+  renderWithRouter(<App />);
+  const title = screen.getByRole('heading', {
+    name: /Encountered Pokémon/i,
+  });
+  expect(title).toBeInTheDocument();
+});
+
+test('testa se aparece só um pokemon', () => {
+  renderWithRouter(<App />);
+
+  const name = screen.getAllByTestId('pokemon-name');
+  expect(name[0]).toBeInTheDocument();
+});
+test('testando tipo do pokemon', () => {
+  renderWithRouter(<App />);
+  const bug = screen.getByRole('button', { name: /bug/i });
+
+  userEvent.click(bug);
+  const caterpie = screen.getByText('Caterpie');
+
+  expect(caterpie).toBeInTheDocument();
+});
+
+test('testando os botoes', () => {
+  renderWithRouter(<App />);
+  const AllPokemon = screen.getByRole('button', {
+    name: /all/i,
+  });
+  userEvent.click(AllPokemon);
+  const typesButtons = screen.getAllByTestId('pokemon-type-button');
+  expect(AllPokemon).toBeInTheDocument();
+  expect(typesButtons[0]).toBeInTheDocument();
+});
+
+test('testando proximo pokemon', () => {
+  renderWithRouter(<App />);
+  const proximo = screen.getByRole('button', {
+    name: /próximo pokémon/i,
+  });
+  userEvent.click(proximo);
+  const charmander = screen.getByText('Charmander');
+  expect(charmander).toBeInTheDocument();
+});
