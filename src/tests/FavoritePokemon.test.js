f12f01f2f4e01c9a3a15afd4b934553ac81e8c6f
Thiago <thiago-470@outlook.com>
2022-11-22 16:33:29 -0300

    requisito 03

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemon } from '../pages';
+
+test('testa se ta exibindo o texto No favorite pokemon found', () => {
+  renderWithRouter(<FavoritePokemon />);
+  const noFavorite = screen.getByText('No favorite Pokémon found');
+  expect(noFavorite).toBeInTheDocument();
+});
